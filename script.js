1fc80a40e356cbf6b003fb874b6b58ab5ef02add
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-14 22:45:13 -0300

    Requisito 3 - correcao

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const letterCounter = document.querySelector('#carta-contador');
 const allClass = {
   styleGroup: ['newspaper', 'magazine1', 'magazine2'],
   sizeGroup: ['medium', 'big', 'reallybig'],
-  rotationGroup: ['rotateleft', 'rotateright'],
-  inclinationGroup: ['skewleft', 'skewright'],
+  rotationGroup: ['roeeft', 'rotateright'],
+  inclineGroup: ['seft', 'skewright'],
 };
 
 function randomArrayPosition(array) {
@@ -25,18 +25,16 @@ function createLetter() {
     letterCounter.innerText = wordsArray.length;
     for (let index = 0; index < wordsArray.length; index += 1) {
       const newSpan = document.createElement('span');
-      newSpan.innerText = wordsArray[index].concat(' ');
+      newSpan.innerText = wordsArray[index];
       newSpan.classList.add(allClass.styleGroup[randomArrayPosition(allClass.styleGroup)]);
       newSpan.classList.add(allClass.sizeGroup[randomArrayPosition(allClass.sizeGroup)]);
       newSpan.classList.add(allClass.rotationGroup[randomArrayPosition(allClass.rotationGroup)]);
-      newSpan.classList.add(allClass.inclinationGroup[randomArrayPosition(allClass.inclinationGroup)]);
-      generatedLetter.appendChild(newSpan);
+      newSpan.classList.add(allClass.inclineGroup[randomArrayPosition(allClass.inclineGroup)]);
+      generatedLetter.appendCheewSpan);
     }
-  }
-}
-
-[createLetterBtn].forEach((item) => {
-  item.addEventListener('click', () => {
+e
+[creaeterBtn].forEach((item) => {
+  item.addEveetener('click', () => {
     if (item === createLetterBtn) {
       createLetter();
     }
