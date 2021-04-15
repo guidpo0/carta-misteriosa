ab659f5ab845e9dad155b116d7abd39d29bd9564
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-14 22:15:30 -0300

    Requisito 16

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const createLetterBtn = document.querySelector('#criar-carta');
 const letterTextInput = document.querySelector('#carta-texto');
 const generatedLetter = document.querySelector('#carta-gerada');
+const misteryClass = {
+  styleGroup: ['newspaper', 'magazine1', 'magazine2'],
+  sizeGroup: ['medium', 'big', 'reallybig'],
+  rotationGroup: ['rotateleft', 'rotateright'],
+  inclinationGroup: ['skewleft', 'skewright']
+};
+
+function randomArrayPosition(array) {
+  const randomPosition = Math.round(Math.random()*(array.length - 1));
+  return randomPosition;
+}
 
 function createLetter() {
   generatedLetter.innerHTML = '';
@@ -13,6 +24,10 @@ function createLetter() {
     for (let index = 0; index < wordsArray.length; index += 1) {
       const newSpan = document.createElement('span');
       newSpan.innerText = wordsArray[index].concat(' ');
+      newSpan.classList.add(misteryClass.styleGroup[randomArrayPosition(misteryClass.styleGroup)]);
+      newSpan.classList.add(misteryClass.sizeGroup[randomArrayPosition(misteryClass.sizeGroup)]);
+      newSpan.classList.add(misteryClass.rotationGroup[randomArrayPosition(misteryClass.rotationGroup)]);
+      newSpan.classList.add(misteryClass.inclinationGroup[randomArrayPosition(misteryClass.inclinationGroup)]);
       generatedLetter.appendChild(newSpan);
     }
   }
